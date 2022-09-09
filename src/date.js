7ce5d8c00f7939b8a3ed3cd735b6e211080f5714
Dom Christie <christiedom@gmail.com>
2022-09-08 18:23:40 -0700

    Screen-reader friendly time elements

--- src/date.js
+++ src/date.js
@@ -1,9 +1,9 @@
 import { format } from 'date-fns'
 
-export function formatDate (dateString) {
+export function formatDate (dateString, pattern = 'dd MMM yyyy') {
   const localDate = new Date(dateString)
   const date = new Date(
     localDate.valueOf() + localDate.getTimezoneOffset() * 60 * 1000
   )
-  return format(new Date(date), 'dd MMM yyyy')
+  return format(new Date(date), pattern)
 }
